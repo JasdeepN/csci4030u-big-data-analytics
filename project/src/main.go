9736410704815108fd605c02fe7c8b81bbe63ae1
JasdeepN <jasdeepn4@hotmail.com>
2016-01-29 12:20:35 -0500

    correctly sorts into bins

--- project/src/main.go
+++ project/src/main.go
@@ -1,28 +1,59 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "os"
+"bufio"
+"fmt"
+"log"
+"os"
+"strings"
 )
 
 func main() {
-    file, err := os.Open("retail.dat")
-    if err != nil {
-        log.Fatal(err)
+
+    var first_pass map [string]int 
+    first_pass = make(map[string]int)
+  //  _ = first_pass
+
+    basket_total := 0
+    threshold := 2 // items that appear >= threshold = frequent items
+
+    _ = threshold // fixes the "declared but not used err"
+
+   file, err := os.Open("small_data.dat") //101 items only
+    //file, err := os.Open("retail.dat") //full data
+
+   if err != nil {
+    log.Fatal(err)
     }
     defer file.Close()
 
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
-        fmt.Println(scanner.Text())
+        // fmt.Println(scanner.Text())
+        count := 0
+        basket_total = basket_total + 1
+        words := strings.Fields(scanner.Text())
+
+        for(count < len(words)){
+            first_pass[words[count]] = first_pass[words[count]] + 1
+            count++ 
+        }
+       
     }
 
     if err := scanner.Err(); err != nil {
         log.Fatal(err)
     }
+
+    fmt.Println("total baskets: ", basket_total)
+    //fmt.Println("Map: ", first_pass)
+    
+    _printMap(first_pass)
+   
 }
 
-//add data structure to save data into buckets
-//do rest
+func _printMap(input map[string]int)  {
+     for key, value := range input {
+        fmt.Println("Key:", key, "Value:", value)
+    }
+}
