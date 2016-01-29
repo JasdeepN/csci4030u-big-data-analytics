821d8c0b2a675fd8cdd79c9f2622747729cb4c49
Jasdeep <jasdeepn4@hotmail.com>
2016-01-29 13:46:18 -0500

    need to fix keeping track of mappint items to new map

--- project/src/main.go
+++ project/src/main.go
@@ -6,40 +6,48 @@ import (
 "log"
 "os"
 "strings"
+"strconv"
 )
 
 func main() {
+    var currMap map [string]int 
+    currMap = make(map[string]int)
 
-    var first_pass map [string]int 
-    first_pass = make(map[string]int)
-  //  _ = first_pass
+    var oldMap map [string]int 
+    oldMap = make(map[string]int)
+    _ = oldMap
 
     basket_total := 0
     threshold := 2 // items that appear >= threshold = frequent items
 
-    _ = threshold // fixes the "declared but not used err"
+   //_ = threshold // fixes the "declared but not used err"
 
-    first_pass, basket_total = _readData("small_data.dat", )
+    currMap, basket_total = _passOne("small_data.dat")
+
+    _printMap(currMap)
+
+    oldMap, currMap = _passTwo(currMap, threshold)
+
+    fmt.Println("oldMap")
+    _printMap(oldMap)
+
+    fmt.Println("currMap")
+    _printMap(currMap)
 
     fmt.Println("total baskets: ", basket_total)
-    //fmt.Println("Map: ", first_pass)
-    
-    _printMap(first_pass)
-
+    //fmt.Println("Map: ", currMap)
 }
 
-func _printMap(input map[string]int)  {
-    for key, value := range input {
+func _printMap(args map[string]int)  {
+    for key, value := range args {
         fmt.Println("Key:", key, "Value:", value)
     }
-
 }
 
-func _readData(input_file string) (map[string]int, int)  {
+func _passOne(input_file string) (map[string]int, int)  {
     var inital_map map [string]int
     inital_map = make(map[string]int)
     basket_total := 0
-
 
     file, err := os.Open(input_file) //101 items only
     //file, err := os.Open("retail.dat") //full data
@@ -60,7 +68,6 @@ func _readData(input_file string) (map[string]int, int)  {
             inital_map[words[count]] = inital_map[words[count]] + 1
             count++ 
         }
-
     }
 
     if err := scanner.Err(); err != nil {
@@ -69,4 +76,24 @@ func _readData(input_file string) (map[string]int, int)  {
     return inital_map, basket_total
 }
 
+func _passTwo(args map[string]int, threshold int) (map[string]int, map[string]int) {
+    var newMap map [string]int 
+    newMap = make(map[string]int)
+    count := 0
+
+    //frequent items
+    for key, value := range args {
+        if value < threshold {
+            delete(args, key)
+        }
+    }
+
+    //making old item map
+    for key := range args {
+        count++
+        newMap[strconv.Itoa(count)] = args[key] 
+    }
+
+    return args, newMap
+}
 
